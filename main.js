0a1568b12226b6a26c43ec527baa04e4e884fcf2
Nourhan-Atef-Hossam-El-Deen <91796430+Nourhaan-Atef@users.noreply.github.com>
2022-11-22 14:27:09 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+
+let n1 = [10, 30, 10, 20];
+let n2 = [30, 20, 10];
+// console.log(Math.max(...n1) * [...n1, ...n2].length) // 210
+console.log(new Set(n1).size *  [...n1 , ...n2].length * Math.min(...n2)) //210
+console.log(Math.min(...n2))
+console.log(...n2)
